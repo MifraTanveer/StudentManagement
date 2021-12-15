10bb7f898ce258f13cfddd5b6835cd70f7e09492
MifraTanveer <mifratanveer09@gmail.com>
2021-12-15 15:25:14 +0500

    First Commit

---
+++ attendanceproj.java
@@ -0,0 +1,11 @@
+
+package project;
+
+// class Attendanceproj which extend other class Entryportal
+public class Attendanceproj extends Entryportal {
+
+    public static void main(String[] args) {
+        Entryportal f1 = new Entryportal();
+        f1.setVisible(true);
+    }
+}
